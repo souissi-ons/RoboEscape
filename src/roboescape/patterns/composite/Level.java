ac088fc2659850f715a3ac7277643a4a5eaf0351
souis <ons.souissi01@gmail.com>
2025-12-01 20:24:54 +0100

    Add Singleton GameLogger for tracing events

--- src/roboescape/patterns/composite/Level.java
+++ src/roboescape/patterns/composite/Level.java
@@ -2,18 +2,27 @@ package roboescape.patterns.composite;
 
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.canvas.GraphicsContext;
 
 public class Level implements LevelComponent {
 
-    private List<LevelComponent> components = new ArrayList<>();
+    private List<LevelComponent> children = new ArrayList<>();
 
-    public void add(LevelComponent comp) {
-        components.add(comp);
+    public void add(LevelComponent c) {
+        children.add(c);
+    }
+
+    public void remove(LevelComponent c) {
+        children.remove(c);
     }
 
     @Override
-    public void render() {
-        for (LevelComponent c : components)
-            c.render();
+    public void render(GraphicsContext gc) {
+        children.forEach(c -> c.render(gc));
+    }
+
+    @Override
+    public void update() {
+        children.forEach(LevelComponent::update);
     }
 }
--- src/roboescape/patterns/composite/Room.java
+++ src/roboescape/patterns/composite/Room.java
@@ -1,8 +1,27 @@
 package roboescape.patterns.composite;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Room implements LevelComponent {
 
+    private double x, y, width, height;
+
+    public Room(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
     @Override
-    public void render() {
+    public void render(GraphicsContext gc) {
+        gc.setStroke(Color.GRAY);
+        gc.setLineWidth(3);
+        gc.strokeRect(x, y, width, height);
+    }
+
+    @Override
+    public void update() {
     }
 }
